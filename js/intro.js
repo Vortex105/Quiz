a0b765565fb4513e1491cbbc494e2286b70e8ac9
Hunsa-wusu Seseshe <seseshe14@gmail.com>
2025-01-01 22:45:02 +0100

    Made some corrections

--- js/intro.js
+++ js/intro.js
@@ -7,13 +7,13 @@ const form = document.getElementById("qForm");
 let yourName = localStorage.getItem("Username");
 entryName.textContent = yourName;
 
-totalQ.forEach(length => length.textContent = questions.length)
+totalQ.forEach(length => length.textContent = localQuestions.length)
 
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    if(entryNum.value == "" || entryNum.value < 10 || entryNum.value > questions.length) {
-        alert("Please Enter a number between 10 and 23");
+    if(entryNum.value == "" || entryNum.value < 10 || entryNum.value > localQuestions.length) {
+        alert(`Please Enter a number between 10 and ${localQuestions.length}`);
         entryNum.value = ""
         return;
     }
